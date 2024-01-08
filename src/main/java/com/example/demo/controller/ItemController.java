6dedf7a106402b116663764cc037701a98ff1b12
villageworks <villageworks@gmail.com>
2024-01-08 20:15:17 +0900

    ver.0.13: カテゴリ別商品一覧画面表示

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.Category;
 import com.example.demo.entity.Item;
@@ -23,13 +24,20 @@ public class ItemController {
 	ItemRepository itemRepository; 			// seq:20.2
 	
 	@GetMapping("/items")	// 20.3
-	public String index(Model model) {
+	public String index(
+			@RequestParam(name = "categoryId", defaultValue = "") Integer categoryId,
+			Model model) {
 		// カテゴリーリストを取得
 		List<Category> categoryList = categoryRepository.findAll(); // seq:20.4
 		// 取得したカテゴリーリストをスコープに登録
 		model.addAttribute("categoryList", categoryList);			// seq:20.5
 		// すべての商品リストを取得
-		List<Item> itemList = itemRepository.findAll(); // seq:20.6
+		List<Item> itemList = null;
+		if (categoryId == null) {
+			itemList = itemRepository.findAll(); // seq:20.6
+		} else {
+			itemList = itemRepository.findByCategoryId(categoryId); // seq:20.7
+		}
 		// 取得した商品リストをスコープに登録
 		model.addAttribute("itemList", itemList);		// seq:20.8
 		// 取得したカテゴリーリストをスコープに登録
