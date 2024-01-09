866759238e45d8bf4b95f0ac0ded23720f50a5fd
villageworks <villageworks@gmail.com>
2024-01-10 00:17:55 +0900

    ver.0.32: 顧客情報確認画面表示

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -2,9 +2,14 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.entity.Customer;
 import com.example.demo.model.Cart;
+
 
 @Controller
 public class OrderController {
@@ -16,5 +21,21 @@ public class OrderController {
 	public String index() {
 		return "customerForm"; // seq:41.3
 	}
+	
+	@PostMapping("/order/confirm") // sqe:42.1
+	public String confirm(
+			@RequestParam(name = "name", defaultValue = "") String name,
+			@RequestParam(name = "address", defaultValue = "") String address,
+			@RequestParam(name = "tel", defaultValue = "") String tel,
+			@RequestParam(name = "email", defaultValue = "") String email,
+			Model model) {
+		// リクエストパラメータをもとに顧客のインスタンスを生成
+		Customer customer = new Customer(name, address, tel, email); // seq:42.2
+		// 生成した顧客インスタンスをスコープに登録
+		model.addAttribute("customer", customer); // seq:42.3
+		// 画面遷移
+		return "customerConfirm"; // seq:42.4
+	}
+	
 	
 }
