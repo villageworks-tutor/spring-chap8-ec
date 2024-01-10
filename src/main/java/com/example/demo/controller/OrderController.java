700bfcd48bfcbb23142da8f16813a04b0bda431a
villageworks <villageworks@gmail.com>
2024-01-10 22:42:00 +0900

    ver.0.42: 注文明細永続化

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,10 +12,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.Customer;
+import com.example.demo.entity.Item;
 import com.example.demo.entity.Order;
+import com.example.demo.entity.OrderDetail;
 import com.example.demo.model.Cart;
 import com.example.demo.repository.CustomerRepository;
+import com.example.demo.repository.OrderDetailRepository;
 import com.example.demo.repository.OrderRepository;
+
+import jakarta.servlet.http.HttpSession;
 
 
 
@@ -23,11 +30,17 @@ public class OrderController {
 	@Autowired // seq:41.1
 	Cart cart;
 	
+	@Autowired // seq:43.1
+	HttpSession session;
+	
 	@Autowired // seq:43.2
 	CustomerRepository customerRepository;
 	
 	@Autowired // seq:43.3
 	OrderRepository orderRepository;
+	
+	@Autowired // seq:43.4
+	OrderDetailRepository detailRepository;
 	
 	@GetMapping("/order")      // seq:41.2
 	public String index() {
@@ -72,8 +85,25 @@ public class OrderController {
 		// 注文インスタンのインスタンス化
 		orderRepository.save(order); // seq:43.12
 		
+		// 注文明細永続化
+		// 注文明細リストの生成
+		List<OrderDetail> detailList = new ArrayList<>(); // seq:43.13
+		Integer orderId = order.getId();                  // seq:43.14
+		// カートの商品数分繰り返す
+		for (Item item : cart.getItems()) {
+			OrderDetail detail = new OrderDetail(orderId, item.getId(), item.getQuantity()); // seq:43.15
+			detailList.add(detail); // seq:43.16
+		}
+		// 注文明細リストの永続化
+		detailRepository.saveAll(detailList);       // seq:43.17
+		
+		// 注文番号をスコープに登録
+		model.addAttribute("orderNumber", orderId); // seq:43.18
+		// カートを初期化
+		cart.clear();                               // seq:43.19
+		
 		// 画面遷移
-		return "ordered"; // seq:43.
+		return "ordered";                           // seq:43.20
 	}
 	
 	
